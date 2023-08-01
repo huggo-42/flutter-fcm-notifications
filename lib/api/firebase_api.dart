5201a2858e98f9c46538d13f334ed7a4a868e5f4
huggo <victorhuggocontato42@gmail.com>
2023-08-01 20:53:21 -0300

    feat/refactor(fcm): new handlers and cleaner code

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_fcm_notifications/main.dart';
+import 'package:flutter_fcm_notifications/pages/notification_screen.dart';
 // top class function -> meaning, doesn't need initialization of a class
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
@@ -6,6 +8,25 @@ Future<void> handleBackgroundMessage(RemoteMessage message) async {
   print('title: ${message.notification?.title}');
   print('body: ${message.notification?.body}');
   print('payload: ${message.data}');
+}
+
+void handleMessage(RemoteMessage? message) {
+  if (message == null) return;
+
+  navigatorKey.currentState
+      ?.pushNamed(NotificationScreen.route, arguments: message);
+}
+
+Future initPushNotifications() async {
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+  FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+  FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
 }
 
 class FirebaseApi {
@@ -16,7 +37,7 @@ class FirebaseApi {
 
     final fcmToken = await _firebaseMessaging.getToken();
     print('fcmToken: $fcmToken');
-    // can not use a anonymous function for onBackgroundMessage
-    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+    initPushNotifications();
   }
 }
