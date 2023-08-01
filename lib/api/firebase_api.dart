12d41c6c0701b496cda813ca2f90627d5de279af
huggo <victorhuggocontato42@gmail.com>
2023-08-01 19:55:35 -0300

    feat(fcm): push notifications for when on background and when exited

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,12 +1,22 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+// top class function -> meaning, doesn't need initialization of a class
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('handleBackgroundMessage: $message');
+  print('title: ${message.notification?.title}');
+  print('body: ${message.notification?.body}');
+  print('payload: ${message.data}');
+}
 
 class FirebaseApi {
-        final _firebaseMessaging = FirebaseMessaging.instance;
+  final _firebaseMessaging = FirebaseMessaging.instance;
 
-    Future<void> initNotifications() async {
-            await _firebaseMessaging.requestPermission();
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
 
-            final fcmToken = await _firebaseMessaging.getToken();
-            print('fcmToken: $fcmToken');
-        }
-    }
+    final fcmToken = await _firebaseMessaging.getToken();
+    print('fcmToken: $fcmToken');
+    // can not use a anonymous function for onBackgroundMessage
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  }
+}
